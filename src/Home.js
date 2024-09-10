a069ee6b1f764d44c24d30aa6436a482d42c47fb
Ankit Kushwah <ankitkushwahji159@gmail.com>
2024-09-10 14:30:59 +0530

    useNavigate-Hook

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,19 @@
-import React from "react"
-const Home = () =>{
+import React from "react";
+import { useNavigate } from "react-router-dom";
+const Home = () => {
+  const naviagte = useNavigate();
+
+  const goContactComponet = () => {
+    naviagte("/contact");
+  };
   return (
     <>
-     <h1> This is a home componet </h1>
+      <h1> This is a home componet </h1>
+      <button onClick={goContactComponet}>
+        <h1>Go Contact Componet</h1>
+      </button>
     </>
-  )
-}
+  );
+};
 
 export default Home;
