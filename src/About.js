630a6942ae8b5a11faba6b16e0e216cdb7f733ec
Ankit Kushwah <ankitkushwahji159@gmail.com>
2024-09-10 12:44:28 +0530

    React Router DOM

---
+++ src/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+const About = () =>{
+  return (
+    <>
+     <h1> this is about componet</h1>
+    </>
+  )
+}
+
+export default About;
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from "react"
+const Home = () =>{
+  return (
+    <>
+     <h1> This is a home componet </h1>
+    </>
+  )
+}
+
+export default Home;
